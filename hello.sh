8a55772b7d0955ef56c1cbdb02da4b21cfc5994a
kapil <kapildulbaji009@gmail.com>
2021-11-14 19:24:56 +0530

    this is my latest commit

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# this  is my first comment
+echo "hello kd" # this is the comment
+
+echo  our bash name is $BASH
+ 
+name=Kapil 
+VALUE300=3000 
+echo the name is $name 
+echo MONEY $VALUE300
---
+++ kp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ echo  ' enter the name:'
+read  -a name
+
+echo " names : ${name[0]} ,${name[1]}"
+
